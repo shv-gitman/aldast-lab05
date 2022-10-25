ab8eeecc1502fa60184d5a0bbce9a9d4c80cc36c
Franck Chauvel <fchauvel@dhcp-10-24-88-76.wlan.ntnu.no>
2022-10-25 09:52:31 +0200

    fix: Broken test

--- src/test/java/no/ntnu/idata2302/lab05/generators/AldousBroderTest.java
+++ src/test/java/no/ntnu/idata2302/lab05/generators/AldousBroderTest.java
@@ -1,10 +1,11 @@
 /*
- * This file is part of NTNU's IDATA2302 Lab02.
+ * This file is part of NTNU's IDATA2302 Lab 05.
  *
  * Copyright (C) NTNU 2022
  * All rights reserved.
  *
  */
+
 
 package no.ntnu.idata2302.lab05.generators;
 
@@ -12,11 +13,12 @@ import no.ntnu.idata2302.lab05.Generator;
 
 import java.util.Random;
 
-public class RandomWalkTest extends GeneratorTest {
+
+public class AldousBroderTest extends GeneratorTest {
 
     @Override
     public Generator generator() {
-        return new RandomWalk(new Random());
+        return new AldousBroder(new Random());
     }
 
 }
