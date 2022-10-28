78d418ec6e2d11b67a70920ee4d07781f2c5a2d9
Franck Chauvel <fchauvel@Francks-MacBook-Air.local>
2022-10-28 06:07:01 +0200

    docs: Adds JavaDoc to the Search class

--- src/main/java/no/ntnu/idata2302/lab05/solvers/Search.java
+++ src/main/java/no/ntnu/idata2302/lab05/solvers/Search.java
@@ -15,6 +15,11 @@ import java.util.LinkedHashMap;
 
 import no.ntnu.idata2302.lab05.Vector;
 
+/**
+ * Represent a data needed when traversing a maze, namely a list of
+ * pending cells (found but not processed) and a set of known cells
+ * (processed).
+ */
 public class Search {
 
     private final LinkedList<Vector> pendingCells;
@@ -25,22 +30,39 @@ public class Search {
         knownCells = new LinkedHashMap<Vector, Record>();
     }
 
+    /**
+     * @return true if there more "pending" cells that needs to be
+     *         processed, null otherwise,
+     */
     boolean hasPendingCells() {
         return !pendingCells.isEmpty();
     }
 
+    /**
+     * @return the first cell in the list of pending cells
+     * @throws IllegalStateException when the list of pending cells is empty
+     */
     Vector firstPending() {
         if (pendingCells.isEmpty())
             throw new IllegalStateException("Empty schedule");
         return pendingCells.removeFirst();
     }
 
+    /**
+     * @return the last cell in the list of pending cells
+     * @throws IllegalStateException when the list of pending cells is empty
+     */
     Vector lastPending() {
         if (pendingCells.isEmpty())
             throw new IllegalStateException("Empty schedule");
         return pendingCells.removeLast();
     }
 
+    /**
+     * @return the closest pending cells found
+     * @throws IllegalStateException is the list of pending cells is
+     *         empty
+     */
     Vector closestPending() {
         if (pendingCells.isEmpty())
             throw new IllegalStateException("Empty schedule");
@@ -60,26 +82,60 @@ public class Search {
         return closest;
     }
 
-    private void showPending() {
-        for (var each : pendingCells) {
-            System.out.println("    " + each + "(d=" + distanceTo(each) + ")");
-        }
-    }
-
-    int distanceTo(Vector position) {
+    /**
+     * @returns the distance to the given cell, according to the
+     *          information collected so far during this search.
+     */
+    private int distanceTo(Vector position) {
         return knownCells.get(position).distance;
     }
 
+    /**
+     * Mark the given cell/position as pending by adding it to the
+     * list of pending cells.
+     *
+     * @param position the cell to mark as pending
+     */
     void markAsPending(Vector position) {
         knownCells.put(position, new Record(position, null, 0));
         pendingCells.add(position);
     }
 
+    /**
+     * Record a new triplet source cell/move/target cell into the
+     * search.
+     *
+     * @param start the cell where we were before to move (as a 2D vector)
+     * @param move  the move we take (as a 2D vector)
+     * @param end   the cell we ended at, also as a 2D Vector
+     *
+     * TODO: This method can be used for all the DFS, BFS
+     * and Dijkstra's algorithm. The point is to look up the given
+     * move, see if we can find the newly found cell 'end' among the
+     * known cells. If the end cell is new, we should add it,
+     * otherwise, we may need to update the distance if we have found
+     * a shorter path.
+     */
     void record(Vector start, Vector move, Vector end) {
         // TODO: Implement this method
         throw new RuntimeException("Not yet implemented.");
     }
 
+    /**
+     * Extract the list of moves that led to the given
+     * position. Useful once we found the exit of the maze return the
+     * actual "solution".
+     *
+     * @param position the move to which we need the list of moves
+     *
+     * @return the list of moves (2D vector) that leads to the given
+     * position
+     *
+     * @throws IllegalArgumentException when the given position is null.
+     *
+     * @throws IllegalStateException if the given is the among the
+     * know cells
+     */
     List<Vector> movesTo(Vector position) {
         if (position == null) {
             throw new IllegalArgumentException("Position cannot be null.");
@@ -87,7 +143,6 @@ public class Search {
         if (!knownCells.containsKey(position)) {
             throw new IllegalStateException("The position " + position + " has not yet been met.");
         }
-
         var solution = new LinkedList<Vector>();
         var current = position;
         var move = knownCells.get(current).move;
@@ -98,6 +153,8 @@ public class Search {
         }
         return solution;
     }
+
+    // --- Private helpers for debug -------------------------------------------
 
     void showSearch() {
         for (var each : knownCells.entrySet()) {
@@ -111,6 +168,12 @@ public class Search {
 
 }
 
+/**
+ * Track the "known" cells. Records contain the starting
+ * position, the move applied, and the best known distance so far. The
+ * end cell can re-computed by applying the "move" to the starting
+ * cell.
+ */
 class Record {
 
     public Vector position;
