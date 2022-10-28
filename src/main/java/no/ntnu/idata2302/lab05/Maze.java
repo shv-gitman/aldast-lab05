2136e7899a6b284540c42c4004a2a611a3c5214b
Franck Chauvel <fchauvel@Francks-MacBook-Air.local>
2022-10-28 06:30:44 +0200

    docs: JavaDocs for the Maze class

--- src/main/java/no/ntnu/idata2302/lab05/Maze.java
+++ src/main/java/no/ntnu/idata2302/lab05/Maze.java
@@ -21,8 +21,15 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+
+/**
+ * Represent a 2D maze, internally with an array of character.
+ */
 public class Maze {
 
+    /**
+     * Reads a maze from the given input stream
+     */
     public static Maze fromStream(InputStream input) throws IOException {
         var reader = new BufferedReader(new InputStreamReader(input));
         var lines = new ArrayList<String>();
@@ -40,6 +47,7 @@ public class Maze {
     public static final Vector SOUTH = new Vector(1, 0);
     public static final Vector WEST = new Vector(0, -1);
 
+    // Set of order of which moves are "explored"
     public static final Vector[] MOVES = new Vector[] {
             NORTH,
             EAST,
@@ -54,6 +62,12 @@ public class Maze {
     private final Vector _entry;
     private final Vector _exit;
 
+    /**
+     * Create a new maze, internally, the maze is a 2D array of
+     * characters.
+     *
+     * @param mazeMap the array of string representing the maze as character
+     */
     public Maze(String[] mazeMap) {
         this.mazeMap = mazeMap;
         this._entry = findMarker(START_MARKER);
@@ -81,27 +95,56 @@ public class Maze {
         throw new RuntimeException("Invalid map: Could not find marker '" + marker + "'");
     }
 
+    /**
+     * @return the dimension of the maze, that, its number of rows and
+     * columns.
+     */
     public Dimension dimension() {
         return new Dimension(fromMapIndex(mazeMap.length - 1),
                              fromMapIndex(mazeMap[0].length() - 1));
     }
 
+    /**
+     * @throws  true is this maze has an exit position
+     */
     public boolean hasExit() {
         return this._exit != null;
     }
 
+    /**
+     * @return the position of the exit associated with this maze, or
+     * null if none exists.
+     */
     public Vector exit() {
         return this._exit;
     }
 
+    /**
+     * @return true if the given position is the exit of this maze
+     * @param position the position to consider
+     */
     public boolean isExit(Vector position) {
         return position.equals(exit());
     }
 
+    /**
+     * @return the entry position of this maze or null if none has
+     * been defined.
+     */
     public Vector entry() {
         return this._entry;
     }
 
+    /**
+     * @return all possible moves at the given position. Not that all
+     * possible moves are the moves that land within the dimension of
+     * this maze, regardless of walls.
+     *
+     * @param start the position where the list of possible moves
+     * should be computed.
+     *
+     * @see validMovesAt
+     */
     public Set<Vector> movesAt(Vector start) {
         var moves = new LinkedHashSet<Vector>();
         for (var anyMove : MOVES) {
@@ -113,6 +156,16 @@ public class Maze {
         return moves;
     }
 
+    /**
+     * @return all the *validMoves* at the given position. Valid moves
+     * are all moves that lands within the dimension of this maze, and
+     * that do not traverse a wall.
+     *
+     * @param position the position where the list of valid moves
+     * should be computed.
+     *
+     * @see movesAt
+     */
     public Set<Vector> validMovesAt(Vector position) {
         var validMoves = new LinkedHashSet<Vector>();
         for (var anyMove : movesAt(position)) {
@@ -123,6 +176,12 @@ public class Maze {
         return validMoves;
     }
 
+    /**
+     * @return true if the given is valid from the given position.
+     *
+     * @param position the 2D vector from where the move should be consider
+     * @param move the 2D vector indicating the moving direction.
+     */
     public boolean isValidAt(Vector position, Vector move) {
         var landing = position.add(move);
         var result = dimension().includes(position)
@@ -131,10 +190,24 @@ public class Maze {
         return result;
     }
 
+    /**
+     * @return true is there is a wall at the given direction (east,
+     * north, sourth, west) for the given cell, false otherwise.
+     *
+     * @param position the position to consider as a 2D vector
+     * @param direction the direction to check
+     */
     public boolean isWall(Vector position, Vector direction) {
         return !isClear(position, direction);
     }
 
+    /**
+     * @return true if there is *no wall* in the given direction at the
+     * selected position, false otherwise
+     *
+     * @param position the position to consider as a 2D vector
+     * @param direction the direction to consider
+     */
     public boolean isClear(Vector position, Vector direction) {
         var marker = markerBetween(position, position.add(direction));
         return Character.isWhitespace(marker)
@@ -142,6 +215,13 @@ public class Maze {
                 || marker == EXIT_MARKER;
     }
 
+    /**
+     * Add a wall around the selected position.
+     *
+     * @param start the position to consider
+     * @param direction the side of the cell where the wall should be
+     * built
+     */
     public void buildWall(Vector start, Vector direction) {
         var marker = '|';
         if (direction.equals(NORTH) || direction.equals(SOUTH)) {
@@ -150,22 +230,45 @@ public class Maze {
         setMarkerBetween(start, start.add(direction), marker);
     }
 
+    /**
+     * Take down the wall at the given position, in the given
+     * direction. Idempotent.
+     *
+     * @param position the position to consider
+     * @param direction the side of the cell whose wall should be
+     * taken down.
+     */
     public void breakWall(Vector position, Vector direction) {
         setMarkerBetween((Vector) position,
                 (Vector) position.add(direction),
                 ' ');
     }
 
+    /**
+     * Print the given maze on the console.
+     */
     public void show() {
         for (String eachRow : mazeMap) {
             System.out.println(eachRow);
         }
     }
 
+    /**
+     * Print this maze using a specific printer object
+     *
+     * @param printer the selected printer object
+     */
     public void print(Printer printer) {
         print(printer, new LinkedList<Vector>());
     }
 
+    /**
+     * Print this maze using selected printer object, together with the given
+     * solution.
+     *
+     * @param printer the printer to user
+     * @param solution the solution to print
+     */
     public void print(Printer printer, List<Vector> solution) {
         var dimension = dimension();
         printer.open(dimension.getColumnCount(), dimension.getRowCount());
